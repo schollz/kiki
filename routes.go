d4ba083509e0b17c48227ea451254c104564cc9b
Zack Scholl <zack.scholl@gmail.com>
2018-01-11 09:42:20 -0800

    Moved server stuff

---
+++ routes.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/schollz/kiki/src/letter"
+)
+
+// GET /img
+func handleImage(c *gin.Context) {
+	AddCORS(c)
+	id := c.Param("id")
+	log.Debugf("fetching image: %s", id)
+	e, err := f.GetEnvelope(id)
+	if err != nil {
+		log.Warn(err)
+		c.Data(http.StatusInternalServerError, "text/plain", []byte(err.Error()))
+		return
+	}
+
+	mimeType := "image/jpeg"
+	if strings.Contains(e.Letter.Purpose, "png") {
+		mimeType = "image/png"
+	}
+
+	imageBytes, err := base64.URLEncoding.DecodeString(e.Letter.Content)
+	if err != nil {
+		c.Data(http.StatusInternalServerError, "text/plain", []byte(err.Error()))
+		return
+	}
+
+	c.Data(http.StatusOK, mimeType, imageBytes)
+}
+
+// POST /letter
+func handleLetter(c *gin.Context) (err error) {
+	AddCORS(c)
+
+	if !strings.Contains(c.Request.RemoteAddr, "127.0.0.1") && !strings.Contains(c.Request.RemoteAddr, "[::1]") {
+		log.Debug(err)
+		return errors.New("must be on local host")
+	}
+
+	// bind the payload
+	var p letter.Letter
+	err = c.BindJSON(&p)
+	if err != nil {
+		log.Debug(err)
+		return
+	}
+	log.Debug(p)
+	err = f.ProcessLetter(p)
+	return
+}
+
+// GET /ping
+func handlePing(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"success": true, "message": f.RegionKey.Public})
+}
+
+// POST /envelope
+func handleEnvelope(c *gin.Context) (err error) {
+	AddCORS(c)
+
+	// bind the payload
+	var p letter.Envelope
+	err = c.BindJSON(&p)
+	if err != nil {
+		return
+	}
+	err = f.ProcessEnvelope(p)
+	return
+}
+
+// GET /list
+func handleList(c *gin.Context) {
+	AddCORS(c)
+	ids, err := f.GetIDs()
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"success": false, "message": err.Error()})
+	} else {
+		c.JSON(http.StatusOK, gin.H{"success": true, "message": "found IDs", "ids": ids, "region_key": f.RegionKey.Public})
+	}
+	return
+}
+
+// GET /download/ID
+// You can always download anything you want but the envelopes are transfered so that the letter is closed up.
+func handleDownload(c *gin.Context) {
+	id := c.Param("id")
+	fmt.Println(id)
+	e, err := f.GetEnvelope(id)
+	// Close up envelope
+	e.Close()
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"success": false, "message": err.Error()})
+	} else {
+		c.JSON(http.StatusOK, gin.H{"success": true, "message": "found envelope", "envelope": e})
+	}
+}
+
+// POST /sync
+func handleSync(c *gin.Context) (err error) {
+	AddCORS(c)
+
+	if !strings.Contains(c.Request.RemoteAddr, "127.0.0.1") && !strings.Contains(c.Request.RemoteAddr, "[::1]") {
+		return errors.New("must be on local host")
+	}
+
+	// bind the payload
+	type Payload struct {
+		Address string `json:"address" binding"required"`
+	}
+	var p Payload
+	err = c.BindJSON(&p)
+	if err != nil {
+		return
+	}
+
+	err = f.Sync(p.Address)
+	return
+}
---
+++ server.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"io"
+	"mime/multipart"
+	"net/http"
+
+	"github.com/gin-contrib/multitemplate"
+	"github.com/gin-gonic/gin"
+	"github.com/schollz/kiki/src/feed"
+	"github.com/schollz/kiki/src/logging"
+	"github.com/schollz/kiki/src/web"
+)
+
+var (
+	// Port defines what port the carrier should listen on
+	Port = "8003"
+	// Location defines where to open up the kiki database
+	Location = "."
+	f        feed.Feed
+	log      = logging.Log
+)
+
+func MiddleWareHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Log request
+		log.Debug(fmt.Sprintf("%v %v %v", c.Request.RemoteAddr, c.Request.Method, c.Request.URL))
+		// Add base headers
+		AddCORS(c)
+		// Run next function
+		c.Next()
+	}
+}
+
+// Run will start the server listening
+func Run() (err error) {
+	// Startup feed
+	log.Debug("opening feed")
+	f, err = feed.Open(Location)
+	if err != nil {
+		log.Debug("feed not found, creating new one")
+		var err2 error
+		f, err2 = feed.New(Location)
+		if err2 != nil {
+			return err2
+		}
+	}
+
+	// Startup server
+	gin.SetMode(gin.ReleaseMode)
+
+	r := gin.New()
+	// Standardize logs
+	r.Use(MiddleWareHandler(), gin.Recovery())
+	r.HTMLRender = loadTemplates("index.tmpl")
+	r.HEAD("/", func(c *gin.Context) { // handler for the uptime robot
+		c.String(http.StatusOK, "OK")
+	})
+	r.GET("/", func(c *gin.Context) {
+		posts, err := f.ShowFeed()
+		if err != nil {
+			log.Error(err)
+		}
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"Posts": posts,
+		})
+	})
+	r.GET("/static/:file", func(c *gin.Context) {
+		file := c.Param("file")
+		filename := "static/" + file
+		data, err := Asset(filename)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "uh-oh")
+		} else {
+			c.Data(http.StatusOK, web.ContentType(filename), data)
+		}
+	})
+	r.GET("/ping", handlePing)
+	r.GET("/img/:id", handleImage)
+	r.POST("/letter", handlerLetter)       // post to put in letter (local only)
+	r.OPTIONS("/letter", handlePing)       // post to put in letter (local only)
+	r.GET("/download/:id", handleDownload) // download a specific envelope
+	r.GET("/list", handleList)             // list all the current envelopes
+	r.POST("/envelope", handlerEnvelope)   // post to put into database (public)
+	r.POST("/sync", handlerSync)           // tell server to sync with another server (local only)
+	r.GET("/test", func(c *gin.Context) {
+		message := ""
+		f.ShowFeed()
+		c.JSON(http.StatusOK, gin.H{"success": err == nil, "message": message})
+	})
+	err = r.Run(":" + Port) // listen and serve on 0.0.0.0:Port
+	return
+}
+
+func respondWithJSON(c *gin.Context, message string, err error) {
+	success := err == nil
+	if !success {
+		message = err.Error()
+	}
+	c.JSON(http.StatusOK, gin.H{"success": success, "message": message})
+}
+
+func handlerLetter(c *gin.Context) {
+	respondWithJSON(c, "letter added", handleLetter(c))
+}
+
+func handlerEnvelope(c *gin.Context) {
+	respondWithJSON(c, "envelope added", handleEnvelope(c))
+}
+
+func handlerSync(c *gin.Context) {
+	respondWithJSON(c, "synced", handleSync(c))
+}
+
+func readFormFile(file *multipart.FileHeader) (data []byte, err error) {
+	src, err := file.Open()
+	if err != nil {
+		return
+	}
+	defer src.Close()
+	buf := bytes.NewBuffer(nil)
+	_, err = io.Copy(buf, src)
+	data = buf.Bytes()
+	return
+}
+
+func AddCORS(c *gin.Context) {
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+}
+
+func loadTemplates(list ...string) multitemplate.Render {
+	r := multitemplate.New()
+	for _, x := range list {
+		templateString, err := Asset("templates/" + x)
+		if err != nil {
+			panic(err)
+		}
+		tmplMessage, err := template.New(x).Parse(string(templateString))
+		if err != nil {
+			panic(err)
+		}
+		r.Add(x, tmplMessage)
+	}
+	return r
+}
