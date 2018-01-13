7f7305aa4d079f73a5b9dcd2a49a8d280bd25bfb
Zack Scholl <zack.scholl@gmail.com>
2018-01-13 09:08:09 -0800

    Fixing things

--- routes.go
+++ routes.go
@@ -75,6 +75,8 @@ func handleEnvelope(c *gin.Context) (err error) {
 func handleList(c *gin.Context) {
 	AddCORS(c)
 	ids, err := f.GetIDs()
+	log.Debug(ids)
+	log.Debug(err)
 	if err != nil {
 		c.JSON(500, gin.H{"status": "error", "error": err.Error()})
 	} else {
