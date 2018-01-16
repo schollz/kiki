fb8fbf0d2bbea0be4718cb9d87b026bcc8f7be56
Zack Scholl <zack.scholl@gmail.com>
2018-01-16 07:02:03 -0500

    Fix logging wrapper

--- src/logging/wrapper.go
+++ src/logging/wrapper.go
@@ -11,7 +11,7 @@ type SeelogWrapper struct {
 	Level string
 }
 
-func (self *SeelogWrapper) init() error {
+func (self *SeelogWrapper) init() (err error) {
 	if "" == self.Level {
 		self.Level = "debug"
 	}
@@ -21,7 +21,7 @@ func (self *SeelogWrapper) init() error {
 	// https://en.wikipedia.org/wiki/ANSI_escape_code#3/4_bit
 	// https://github.com/cihub/seelog/wiki/Log-levels
 	appConfig := `
-	<seelog minlevel="` + Level + `">
+	<seelog minlevel="` + self.Level + `">
 	<outputs formatid="stdout">
 	<filter levels="debug,trace">
 		<console formatid="debug"/>
@@ -48,12 +48,8 @@ func (self *SeelogWrapper) init() error {
 	</seelog>
 	`
 
-	logger, err := seelog.LoggerFromConfigAsBytes([]byte(appConfig))
-	if err != nil {
-		return err
-	}
-	self.Log = logger
-	return nil
+	self.Log, err = seelog.LoggerFromConfigAsBytes([]byte(appConfig))
+	return
 }
 
 func (self *SeelogWrapper) isValidLevel(level string) bool {
