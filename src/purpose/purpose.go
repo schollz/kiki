80be2ddcd20ff55cabb68dfcbbcc36d49ab78f7a
Zack Scholl <zack.scholl@gmail.com>
2018-01-17 15:33:19 -0500

    Update purposes of letters

--- src/purpose/purpose.go
+++ src/purpose/purpose.go
@@ -3,35 +3,49 @@ package purpose
 // There are the available purposes for a letter
 var (
 	// Share text will share a text post
+	// Content: HTML
 	ShareText = "share-text"
 
 	// SharePNG shares a PNG image
+	// Content: Base64 of image
 	SharePNG = "share-image/png"
 
 	// ShareJPG shares a JPG image
+	// Content: Base64 of image
 	ShareJPG = "share-image/jpg"
 
 	// ShareKey is for sharing keypairs with friends, or with self
+	// Content: Marshalled keypair.KeyPair
 	ShareKey = "share-key"
 
-	// Actionments are always public
+	// Actions are always public
 
 	// ActionFollow will follow someone
+	// Content: Public key of the person being followed
 	ActionFollow = "action-follow"
 
 	// ActionLike will give a person a like
+	// Content: ID of the post being liked
 	ActionLike = "action-like"
 
 	// ActionName will assign a person a name
+	// Content: Text of name
 	ActionName = "action-assign/name"
 
+	// ActionProfile will assign a person a profile
+	// Content: HTML of profile
 	ActionProfile = "action-assign/profile"
 
+	// ActionProfile will assign a person a profile image
+	// Content: ID of the image letter
 	ActionImage = "action-assign/image"
 
+	// ActionBlock will block a person
+	// Content: Public key of the person to block
 	ActionBlock = "action-block"
 
 	// ActionErase will erase a persons profile from every carrier
+	// Content: Empty
 	ActionErase = "action-erase"
 )
 
